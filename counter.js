d3390441aee0d1b8570874bc459f3fa468843ed3
Saheed Oladosu <oladosu777@gmail.com>
2019-01-18 10:50:42 +0100

    resolved the logic issue

--- counter.js
+++ counter.js
@@ -1,6 +1,10 @@
 const counter = (string, char) =>{
 	let count = 0;
-	for(let j = 0; j < string.lenght; j++){
-		if()
+	for(let j = 0; j < string.length; j++){
+		if(string[j] == char){
+		  count += 1;
+		}
 	}
+	return count;
 }
+console.log(counter('Iamtoogoodtobelazyo', 'o'))
